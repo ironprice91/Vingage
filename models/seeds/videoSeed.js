ad68079c831ecbd4c00f24457c9b2b7d6ca27b56
Michael Canlas <michael.canlas428@gmail.com>
2014-11-11 17:47:15 -0700

    fixed seed

--- models/seeds/videoSeed.js
+++ models/seeds/videoSeed.js
@@ -4,12 +4,12 @@ var Video = require('../video.js');
 Video.find({}, function(err, results){
 	if(results.length === 0){
 		var videoGuy = new Video({
-			videoSrc: 'https://s3.amazonaws.com/refactoru/public/bunny.mp4',
 			title: 'Bunny',
+			videoSrc: 'https://s3.amazonaws.com/refactoru/public/bunny.mp4',
 			notes: [{
 				time: 6,
 				displayTime: "0:06",
-				note: ['This is a note I want on the page']
+				note: 'This is a note I want on the page'
 			}]
 		});
 	videoGuy.save();
